4f4b0937b879e35525653d2fe7d453654c2bdc4c
PoulomiC <73661722+PoulomiC@users.noreply.github.com>
2021-04-07 18:12:08 +0530

    Update sketch.js
    Added boxX and boxY variables. Initialized them with values. Also adjusted the positions of boxSprites to align them for package. 
    Indented the code and commented off unnecessary lines. Please merge these changes and check.

--- sketch.js
+++ sketch.js
@@ -1,21 +1,23 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+var helicopterIMG, helicopterSprite, packageSprite, packageIMG;
+var packageBody, ground, boxX=0, boxY=0;
+
+function preload(){
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
 
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(windowWidth, 600);
 	rectMode(CENTER);
 	
+	boxX = width/2;
+	boxY = 600;
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
@@ -25,7 +27,7 @@ function setup() {
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite=createSprite(width/2, height, width, 18);
 	groundSprite.shapeColor=color(255)
 
 
@@ -37,31 +39,27 @@ function setup() {
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, height-35, width, 10 , {isStatic:true});
  	World.add(world, ground);
 
-
- 	boxleftSprite=createSprite(boxPosition+100, boxY+45-20, 20,100);
+ 	boxleftSprite=createSprite(boxX-80, boxY-85, 20, 150);
  	boxleftSprite.shapeColor=color(255,0,0);
 
- 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxLeftBody);
+ 	/*boxLeftBody = Bodies.rectangle(boxX, boxY-15, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);*/
 
- 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase=createSprite(boxX+10, boxY-20, 200,20);
  	boxBase.shapeColor=color(255,0,0);
 
- 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
- 	World.add(world, boxBottomBody);
+ 	/*boxBottomBody = Bodies.rectangle(boxX+10, boxY-15, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);*/
 
- 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
+ 	boxRightSprite=createSprite(boxX+100 , boxY-85, 20, 150);
+ 	boxRightSprite.shapeColor=color(255,0,0);
 
- 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxRightBody);
-
-
-	Engine.run(engine);
-  
+ 	/*boxRightBody = Bodies.rectangle(boxX+200-20 , boxY-15, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);*/
+	Engine.run(engine);  
 }
 
 
@@ -69,18 +67,14 @@ function draw() {
   rectMode(CENTER);
   background(0);
  
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
   
   drawSprites();
 }
    
-function keyPressed(){
-	
-	if (keyCode --- DOWN_ARROW){
+function keyPressed(){	
+	if (keyCode === DOWN_ARROW){
 		Matter.Body.setStatic(packageBody,false);
 	}
-
-
 }
